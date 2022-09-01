9c26cd4f05bf63338793b7460958beeae66190f6
kaibalya jena <kaibalya18j@gmail.com>
2022-09-01 10:14:55 +0530

    screen files

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/screens/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,8 +8,9 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "firebase",
-      home: Scaffold(),
+      home: LoginPage(),
     );
   }
 }
